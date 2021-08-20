20d33540bbb1046017571c25aea053fd06776bea
chronoviser <abhisheksinghoriginal@gmail.com>
2021-08-21 00:30:54 +0530

    Updated Landing Page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import BirthdayApp from './Birthday-Reminder/App';
 import TourBookApp from './Tour-Book/App';
+import Styled from 'styled-components';
+
+const StyledLink = Styled(Link)`
+  margin: 0 30% 0 30%;
+  display: block;
+  font-size: 24px;
+  background: azure;
+  font-weight: 600;
+  padding: 15px;
+  text-decoration: none;
+  color: matte;
+  cursor: pointer;
+  border-bottom: 4px solid #fff;
+  transition: color 0.25s ease-in;
+
+  &:hover {
+    color: #777;
+  }
+`
+
 
 function App() {
   return (
     <div className="App">
       <Route path='/' exact render={() =>
         <>
-          <h2>Projects Boquete</h2>
-          <Link to="/birthday-reminder-app" exact>Birthday Reminder App</Link>
-          <Link to="/tour-book-app" exact>Tour Book App</Link>
+          <h1>Projects Boquete</h1>
+          <StyledLink to="/birthday-reminder-app" exact>Birthday Reminder App</StyledLink>
+          <StyledLink to="/tour-book-app" exact>Tour Book App</StyledLink>
         </>
       } />
       <Route path="/birthday-reminder-app" component={BirthdayApp} />
